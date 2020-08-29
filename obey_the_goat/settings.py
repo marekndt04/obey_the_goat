aaaf4718dad6b29cdf8d9e61c67d65e4025b0a20
Marek Arendt <marek.arendt@yougov.com>
2020-08-29 11:13:13 +0200

    4.4 Refactor home page view to use a template

--- obey_the_goat/settings.py
+++ obey_the_goat/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lists'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'obey_the_goat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'lists/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
