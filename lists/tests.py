c3b6b2fb18bd50eaddf9d5fd650fffb92ae60016
Marek Arendt <marek.arendt@yougov.com>
2020-10-25 12:37:07 +0100

    5.10 Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -11,9 +11,27 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home_page.html')
 
     def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual('A new list item', new_item.text)
+
+    def test_redirects_after_POST(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home_page.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTests(TestCase):
@@ -34,3 +52,7 @@ class ItemModelTests(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'First (ever) item')
         self.assertEqual(second_saved_item.text, 'Second item')
+
+    def test_only_save_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.shortcuts import render
 
+from lists.models import Item
 
-# Create your views here.
+
 def home_page(request):
-    return render(request, 'home_page.html', {
-        'new_item_text': request.POST.get('item_text', '')
-    })
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home_page.html', {'items': items})
