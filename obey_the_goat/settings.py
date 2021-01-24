a53aa6786f9f8f101782128f7d2125469c4fe45c
Marek Arendt <marek.arendt@yougov.com>
2021-01-24 09:41:11 +0100

    10.6 use env vars for prod settings DEBUG, ALLOWED_HOSTS, SECRET_KEY

--- obey_the_goat/settings.py
+++ obey_the_goat/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b18clj%egm906_n-m-^akj)sslfnr7kw9)58j2p-bg-_#h=qws'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['https://obey-the-goat.herokuapp.com/']
-
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = os.environ['SITENAME']
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
 
 # Application definition
 
